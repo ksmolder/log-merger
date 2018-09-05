81591f6fa5c13861c1da6a9da9abec182ea6cfce
Kurt Smolderen <kurt.smolderen@empuly.net>
2018-09-06 00:01:30 +0200

    Format with relaxed line wrapping

--- src/test/java/logmerge/LogFileTest.java
+++ src/test/java/logmerge/LogFileTest.java
@@ -18,51 +18,51 @@ import static org.junit.Assert.assertThat;
 
 public class LogFileTest {
 
-    @Test
-    public void testWildflyServerLog() throws IOException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss,SSS");
-        Path path = Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "wildfly_server.log");
-        BufferedReader br = new BufferedReader(new FileReader(path.toFile()));
-        CliOptions cliOptions = new CliOptions();
-        cliOptions.setDelimiter(" ");
-        cliOptions.setFieldNumber(new int[]{1, 2});
-        LogFile logFile = new LogFile(path.toString(), br, sdf, cliOptions, 0);
-        Date date = logFile.peekNextTimestamp();
-        assertThat(date, is(createDate(29, 8, 2015, 14, 15, 30, 472)));
-        Iterator<String> iterator = logFile.getNextLines();
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), not(nullValue()));
-        date = logFile.peekNextTimestamp();
-        assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 44)));
-        iterator = logFile.getNextLines();
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), not(nullValue()));
-        date = logFile.peekNextTimestamp();
-        assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 103)));
-        iterator = logFile.getNextLines();
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), not(nullValue()));
-        date = logFile.peekNextTimestamp();
-        assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 104)));
-        iterator = logFile.getNextLines();
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), not(nullValue()));
-        date = logFile.peekNextTimestamp();
-        assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 104)));
-        iterator = logFile.getNextLines();
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), not(nullValue()));
-    }
+	@Test
+	public void testWildflyServerLog() throws IOException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss,SSS");
+		Path path = Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "wildfly_server.log");
+		BufferedReader br = new BufferedReader(new FileReader(path.toFile()));
+		CliOptions cliOptions = new CliOptions();
+		cliOptions.setDelimiter(" ");
+		cliOptions.setFieldNumber(new int[] { 1, 2 });
+		LogFile logFile = new LogFile(path.toString(), br, sdf, cliOptions, 0);
+		Date date = logFile.peekNextTimestamp();
+		assertThat(date, is(createDate(29, 8, 2015, 14, 15, 30, 472)));
+		Iterator<String> iterator = logFile.getNextLines();
+		assertThat(iterator.hasNext(), is(true));
+		assertThat(iterator.next(), not(nullValue()));
+		date = logFile.peekNextTimestamp();
+		assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 44)));
+		iterator = logFile.getNextLines();
+		assertThat(iterator.hasNext(), is(true));
+		assertThat(iterator.next(), not(nullValue()));
+		date = logFile.peekNextTimestamp();
+		assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 103)));
+		iterator = logFile.getNextLines();
+		assertThat(iterator.hasNext(), is(true));
+		assertThat(iterator.next(), not(nullValue()));
+		date = logFile.peekNextTimestamp();
+		assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 104)));
+		iterator = logFile.getNextLines();
+		assertThat(iterator.hasNext(), is(true));
+		assertThat(iterator.next(), not(nullValue()));
+		date = logFile.peekNextTimestamp();
+		assertThat(date, is(createDate(29, 8, 2015, 14, 15, 31, 104)));
+		iterator = logFile.getNextLines();
+		assertThat(iterator.hasNext(), is(true));
+		assertThat(iterator.next(), not(nullValue()));
+	}
 
-    private Date createDate(int day, int month, int year, int hour, int minute, int second, int ms) {
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.set(Calendar.DAY_OF_MONTH, day);
-        gc.set(Calendar.MONTH, month - 1);
-        gc.set(Calendar.YEAR, year);
-        gc.set(Calendar.HOUR_OF_DAY, hour);
-        gc.set(Calendar.MINUTE, minute);
-        gc.set(Calendar.SECOND, second);
-        gc.set(Calendar.MILLISECOND, ms);
-        return gc.getTime();
-    }
+	private Date createDate(int day, int month, int year, int hour, int minute, int second, int ms) {
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.set(Calendar.DAY_OF_MONTH, day);
+		gc.set(Calendar.MONTH, month - 1);
+		gc.set(Calendar.YEAR, year);
+		gc.set(Calendar.HOUR_OF_DAY, hour);
+		gc.set(Calendar.MINUTE, minute);
+		gc.set(Calendar.SECOND, second);
+		gc.set(Calendar.MILLISECOND, ms);
+		return gc.getTime();
+	}
 }
--- src/test/java/logmerge/cli/CliParserTest.java
+++ src/test/java/logmerge/cli/CliParserTest.java
@@ -12,26 +12,27 @@ import static org.junit.Assert.*;
 
 public class CliParserTest {
 
-    @Test
-    public void test() throws IOException {
-        Path pathLog1 = Paths.get(System.getProperty("user.dir"), "target", "log1");
-        Path pathLog2 = Paths.get(System.getProperty("user.dir"), "target", "log2");
-        if (!Files.exists(pathLog1)) {
-            Files.createFile(pathLog1);
-        }
-        if (!Files.exists(pathLog2)) {
-            Files.createFile(pathLog2);
-        }
-        CliParser cliParser = new CliParser();
-        CliOptions cliOptions = cliParser.parse(new String[]{"-f", "1", "-d", " ", "-i", "target/log1,target/log2", "-o", "out.log", "-m", "-v"});
-        assertThat(cliOptions.getDelimiter(), is(" "));
-        assertThat(cliOptions.getFieldNumber().length, is(1));
-        assertThat(cliOptions.getFieldNumber()[0], is(1));
-        assertThat(cliOptions.getLogFiles().size(), is(2));
-        assertThat(cliOptions.getLogFiles().get(0), is("target/log1"));
-        assertThat(cliOptions.getLogFiles().get(1), is("target/log2"));
-        assertThat(cliOptions.getOutputFile(), is("out.log"));
-        assertThat(cliOptions.isMarker(), is(true));
-        assertThat(cliOptions.isVerbose(), is(true));
-    }
+	@Test
+	public void test() throws IOException {
+		Path pathLog1 = Paths.get(System.getProperty("user.dir"), "target", "log1");
+		Path pathLog2 = Paths.get(System.getProperty("user.dir"), "target", "log2");
+		if (!Files.exists(pathLog1)) {
+			Files.createFile(pathLog1);
+		}
+		if (!Files.exists(pathLog2)) {
+			Files.createFile(pathLog2);
+		}
+		CliParser cliParser = new CliParser();
+		CliOptions cliOptions = cliParser.parse(
+				new String[] { "-f", "1", "-d", " ", "-i", "target/log1,target/log2", "-o", "out.log", "-m", "-v" });
+		assertThat(cliOptions.getDelimiter(), is(" "));
+		assertThat(cliOptions.getFieldNumber().length, is(1));
+		assertThat(cliOptions.getFieldNumber()[0], is(1));
+		assertThat(cliOptions.getLogFiles().size(), is(2));
+		assertThat(cliOptions.getLogFiles().get(0), is("target/log1"));
+		assertThat(cliOptions.getLogFiles().get(1), is("target/log2"));
+		assertThat(cliOptions.getOutputFile(), is("out.log"));
+		assertThat(cliOptions.isMarker(), is(true));
+		assertThat(cliOptions.isVerbose(), is(true));
+	}
 }
